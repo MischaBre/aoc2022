4d9eae8cc8e48b9cefcbf2a02602fabac67d89f5
gemsa <gemsa.michael@gmail.com>
2022-12-03 13:55:21 +0100

    day1

---
+++ src/day1.py
@@ -0,0 +1,21 @@
+# day1 of aoc2022
+
+def day1() -> int:
+    with open('../input/day1.txt', 'r') as f:
+        elves_cal = list()
+        cal_sum = 0
+        for l in f:
+            if l == '\n':
+                elves_cal.append(cal_sum)
+                cal_sum = 0
+            else:
+                cal_sum += int(l)
+        print(sorted(elves_cal, reverse=True))
+        # part 1
+        # return max(elves_cal)
+        # part 2
+        return sum(sorted(elves_cal, reverse=True)[0:3])
+
+
+if __name__ == '__main__':
+    print(day1())
